34ec60d0c2c70adbec38312e3edd4ae281d9683a
JASPREET SINGH <heroe3270@gmail.com>
2026-01-08 12:20:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import helper as hp
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Auto EDA", layout="wide")
 st.title("Automatic EDA Dashboard")
 data = st.file_uploader(label='select file',type = 'csv')
 
-
 if data is not None:
     data = pd.read_csv(data)
+    data = data.dropna(axis=1, how='all')
     tab1, tab2, tab3 = st.tabs(["Analysis", "Outliers","QQPlot"])
     num_col = []
+    label = []
     cat_col = []
     for i in data.columns:
         if data[i].dtype in ['int64','float64','float32']:
             num_col.append(i)
+            if data[i].nunique() < 3:
+                label.append(i)
         else:
             cat_col.append(i)
     num_df = data[num_col]
@@ -27,12 +28,10 @@ if data is not None:
 
         #Univariate Analysis Tab
         with tab11:
-            col1, col2, col3 = st.columns(3)
+            col1, col2 = st.columns([3,1])
             with col1:
                 hp.h_plot(data)
             with col2:
-                hp.Count(data)
-            with col3:
                 hp.info(data)
 
         # Bivariate analysis
@@ -52,23 +51,23 @@ if data is not None:
         col1, col2, col3 = st.columns(3)
         with col1:
             st.write('Before IQR')
-            hp.h_plot(data,type = 'num')
+            hp.h_plot(num_df)
 
         with col2:
-            st.write('After IQR')
+            st.write('After IQR and Filling Missing Values')
             hp.iqr(num_df)
 
         with col3:
             st.write('Change in Data [Old - New]')
-            hp.iqr(num_df,change = True)
+            hp.iqr(num_df,table = True)
 
     with tab3:
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
 
         with col1:
             st.write('Before Yeo-Johnson')
-            hp.qq_plot(num_df)
+            hp.qq_plot(num_df.drop(columns = label))
 
         with col2:
             st.write('After Yeo-Johnson')
-            hp.qq_plot(num_df,yeo=True)
+            hp.qq_plot(num_df.drop(columns = label),yeo=True)
