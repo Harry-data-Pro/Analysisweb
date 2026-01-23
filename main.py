13f91ac020dab40919697c738966f6e2facff7ea
JASPREET SINGH <heroe3270@gmail.com>
2026-01-23 18:58:03 +0530

    Refactor EDA dashboard with missing values and normalization

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 import helper as hp
 import streamlit as st
 import pandas as pd
@@ -9,7 +11,7 @@ data = st.file_uploader(label='select file',type = 'csv')
 if data is not None:
     data = pd.read_csv(data)
     data = data.dropna(axis=1, how='all')
-    tab1, tab2, tab3 = st.tabs(["Analysis", "Outliers","QQPlot"])
+    tab1, tab2, tab3, tab4 = st.tabs(["Analysis", 'Missing Values',"Outliers",'Normalization'])
     num_col = []
     label = []
     cat_col = []
@@ -22,6 +24,7 @@ if data is not None:
             cat_col.append(i)
     num_df = data[num_col]
     cat_df = data[cat_col]
+    # new_df = hp.impute_missing_values(data,full = True)
 
     with tab1:
         tab11, tab12 = st.tabs(["Univariate analysis", "Bivariate analysis"])
@@ -46,28 +49,65 @@ if data is not None:
             with col3:
                 hp.num_cat_plot(data)
 
-
     with tab2:
+        col1, col2 = st.columns(2)
+        with col1:
+            new_num_df,n_cols = hp.impute_missing_values(num_df)
+            hp.h_plot(new_num_df[n_cols])
+        with col2:
+            new_col_df,c_cols = hp.impute_missing_values(cat_df)
+            hp.h_plot(new_col_df[c_cols])
+
+    with tab3:
         col1, col2, col3 = st.columns(3)
         with col1:
             st.write('Before IQR')
-            hp.h_plot(num_df)
+            hp.h_plot(new_num_df)
 
         with col2:
             st.write('After IQR and Filling Missing Values')
-            hp.iqr(num_df)
+            new_num_df = hp.iqr(new_num_df,df = True)
+            hp.iqr(new_num_df)
 
         with col3:
             st.write('Change in Data [Old - New]')
-            hp.iqr(num_df,table = True)
+            hp.iqr(new_num_df,table = True)
 
-    with tab3:
+    with tab4:
         col1, col2 = st.columns(2)
-
         with col1:
-            st.write('Before Yeo-Johnson')
-            hp.qq_plot(num_df.drop(columns = label))
-
+            # st.write('After Scaling')
+            new_num_df = hp.normalization(new_num_df.select_dtypes(include = 'number'))
+            st.dataframe(new_num_df.sample(5))
         with col2:
-            st.write('After Yeo-Johnson')
-            hp.qq_plot(num_df.drop(columns = label),yeo=True)
+            for i in new_num_df.columns:
+                fig,ax = plt.subplots()
+                num_df[i].plot(kind="kde",ax = ax,label="Original", color="blue")
+                new_num_df[i].plot(kind = 'kde',ax = ax,label="Normalized", color="red")
+                ax.set_xlabel(i)
+                ax.legend()
+                st.pyplot(fig)
+
+
+    new_df = pd.concat([new_num_df, new_col_df, data[label]], axis=1)
+    if 'new_df' in locals():
+        csv_data = new_df.to_csv(index=False).encode('utf-8')
+
+        st.download_button(
+            label="Download Cleaned CSV",
+            data=csv_data,
+            file_name="cleaned_data.csv",
+            mime="text/csv",
+        )
+    else:
+        st.info("Complete the missing value handling in Tab 2 to enable download.")
+    # with tab3:#wait
+    #     col1, col2 = st.columns(2)
+    #
+    #     with col1:
+    #         st.write('Before Yeo-Johnson')
+    #         hp.qq_plot(num_df.drop(columns = label))
+    #
+    #     with col2:
+    #         st.write('After Yeo-Johnson')
+    #         hp.qq_plot(num_df.drop(columns = label),yeo=True)
